f17b7458d64adae605c1c5bc239b1a163a85f41c
ImprovePacoCarlos <proyectodipra@gmail.com>
2017-08-28 17:19:39 -0500

    PDF V1
    Se subieron archivos

---
+++ views/v_pdfgenerator/footer.php
@@ -0,0 +1,53 @@
+
+ 
+ 
+ 
+  <!--Footer-->
+  <footer class="page-footer center-on-small-only elegant-color-dark mt-5">
+
+     
+        <hr>
+
+        <!--Call to action-->
+        <div class="call-to-action">
+            <h4 class="mb-2">DIRECCION EN PREVENCION DE RIESGOS Y ASESORIA FINANCIERA</h4>
+        </div>
+        <!--/.Call to action-->
+
+        <!--Copyright-->
+        <div class="footer-copyright">
+            <div class="container-fluid">
+                © 2017 Copyright: <a href="https://www.MDBootstrap.com"> dipra.com.mx </a>
+
+            </div>
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
+
+    
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="<?php echo base_url();?>/assets/js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>/assets/js/tether.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="<?php echo base_url();?>/assets/js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url();?>/assets/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url();?>/assets/js/mdb.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@1.1.2 "></script>
+    <script  type="text/javascript" src='<?php echo base_url();?>/assets/js/graficas/grafica.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.debug.js"></script>
+    <script  type="text/javascript" src='<?php echo base_url();?>/assets/js/html2canvas.min.js'></script>
+    <script  type="text/javascript" src='<?php echo base_url();?>/assets/js/graficas/generarpdf.js'></script>
+
+
+
+</body>
+
+</html>
